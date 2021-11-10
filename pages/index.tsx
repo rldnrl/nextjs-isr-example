91b1960d2a73c45f6d2d58445b43f3dc424a12fa
Park Kiwook <81848094+vueveloper@users.noreply.github.com>
2021-11-10 10:19:09 +0900

    feat: implement isr example code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styled from "@emotion/styled";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useQuery } from "react-query";
 
-const Home: NextPage = () => {
+import getTodos, { Todo } from "../api/getTodos";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage<{ todos: [Todo] }> = (props) => {
+  const { data: todos } = useQuery("todos", getTodos, {
+    initialData: props.todos,
+  });
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,60 +22,49 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+        {todos?.map((todo) => (
+          <TodoItem
+            href={`/todos/${todo.id}`}
+            key={todo.id}
+            completed={todo.completed}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+            <a>
+              {todo.id} - {todo.title}
+            </a>
+          </TodoItem>
+        ))}
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
+        <span>
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
-        </a>
+        </span>
       </footer>
     </div>
-  )
+  );
+};
+
+export async function getStaticProps() {
+  console.log("Generating / ReGenerating");
+  const todos = await getTodos();
+
+  return {
+    props: {
+      todos,
+    },
+    revalidate: 10,
+  };
 }
 
-export default Home
+type TodoItemProps = {
+  completed: boolean;
+};
+
+const TodoItem = styled(Link)<TodoItemProps>`
+  text-decoration: ${({ completed }) => (completed ? "line-through" : "none")};
+`;
+
+export default Home;
